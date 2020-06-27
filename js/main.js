7b52ad06d5ab34ca18be7d0c7b393bb6f5392d02
MightyVeeMedia <66276318+MightyVeeMedia@users.noreply.github.com>
2020-06-27 13:12:40 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,35 +1,17 @@
-  function latestvids(){
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<script>
-    var reqURL = "https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent("https://www.youtube.com/feeds/videos.xml?channel_id=");
-function loadVideo(iframe) {
-  $.getJSON(reqURL + iframe.getAttribute('cid'),
-    function(data) {
-      var videoNumber = (iframe.getAttribute('vnum') ? Number(iframe.getAttribute('vnum')) : 0);
-      console.log(videoNumber);
-      var link = data.items[videoNumber].link;
-      id = link.substr(link.indexOf("=") + 1);
-      iframe.setAttribute("src", "https://youtube.com/embed/" + id + "?controls=0&autoplay=0");
-    }
-  );
-}
-var iframes = document.getElementsByClassName('latestVideoEmbed');
-for (var i = 0, len = iframes.length; i < len; i++) {
-  loadVideo(iframes[i]);
-}
-    </script>
+/*Banned Users Script */
+function validateForm() {
+  var x = document.forms["myForm"]["name"].value;
+  if (x == "Brian Lyons" || x == "brianlyons" || x == "BrianLyons") {
+    alert("We cannot currently process this donation.");
+    return false;
+  }
 }
 
-
-function ganalytics(){
-  
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-169305111-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-169305111-1');
-</script>
-  
+/*Banned Emails Script*/
+function validateForm2() {
+  var x = document.forms["myForm"]["email"].value;
+  if (x == "AwesomeBro172@gmail.com" || x == "awesomebro172@gmail.com") {
+    alert("We cannot currently process this donation.");
+    return false;
+  }
 }
