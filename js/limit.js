b93525bbb674387e97b0c70b93f1dee8fb36a119
MightyVeeMedia <66276318+MightyVeeMedia@users.noreply.github.com>
2020-11-13 14:47:11 -0500

    Update limit.js

--- js/limit.js
+++ js/limit.js
@@ -1,7 +1,7 @@
 /*Limit Reached Script */
 function validateForm() {
   var x = document.forms["reqform"]["os2"].value;
-  if (x.includes("Lyons") || x.includes("SoniKKnine")) {
+  if (x.includes("Lyons") || x.includes("duf")) {
     alert("Oops! Request limit has been reached! Try again next week!");
     return false;
   }
