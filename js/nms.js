a654cfdbd1a2e0f78d41f5bd5e1255ce5647f743
MightyVeeMedia <66276318+MightyVeeMedia@users.noreply.github.com>
2020-11-13 23:00:43 -0500

    Create nms.js

---
+++ js/nms.js
@@ -0,0 +1,8 @@
+/*Unusable Names Script */
+function validateForm() {
+  var x = document.forms["reqform"]["os2"].value;
+  if (x.includes("BBT") || x.includes("Billy Bob Tanley") || x.includes("BBT Network") || x.includes("ThisBarry") || x.includes("This Barry") || x.includes("Otis") || x.includes("Bubba Dunk Funkin") || x.includes("BDF") || x.includes("BDF Network") || x.includes("Steve the Darkness")|| x.includes("STD")|| x.includes("Green Psycho 17")|| x.includes("Green Psycho")|| x.includes("Jeffrey Gaggens")|| x.includes("JeffreyGaggens")|| x.includes("Proto Vee")|| x.includes("ProtoVee")|| x.includes("Proto Mike V")|| x.includes("ProtoMikeV")|| x.includes("ProtoV")|| x.includes("BillyBobTanley")) {
+    alert("The name entered cannot be used.");
+    return false;
+  }
+}
