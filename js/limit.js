f32f7d311206516c17c0305b3fee5558774bdd8f
MightyVeeMedia <66276318+MightyVeeMedia@users.noreply.github.com>
2021-08-05 11:15:04 -0400

    Update limit.js

--- js/limit.js
+++ js/limit.js
@@ -1,8 +1,8 @@
 /*Limit Reached Script */
 function validateForm() {
   var x = document.forms["reqform"]["os2"].value;
-  if (x.includes("Lyons") || x.includes("duf")) {
-    alert("Oops! Request limit has been reached! Try again next week!");
+  if (x.includes("Lyons") || x.includes("duf")) || x.includes("Augello")) || x.includes("augello")) {
+    alert("Oops! Request could not be processed! Try again later!");
     return false;
   }
 }
