651bd153109d7cf11b4db314f1bd57cf8c086680
MightyVeeMedia <66276318+MightyVeeMedia@users.noreply.github.com>
2025-04-28 11:50:52 -0400

    Delete js/limit.js

--- js/limit.js
+++
@@ -1,8 +0,0 @@
-/*Limit Reached Script */
-function validateForm() {
-  var x = document.forms["reqform"]["os2"].value;
-  if (x.includes("Lyons") || x.includes("duf") || x.includes("Augello") || x.includes("augello")) {
-    alert("Oops! Request could not be processed! Try again later!");
-    return false;
-  }
-}
